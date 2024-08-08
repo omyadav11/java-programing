871503f02cbd95d7684a9406a18a365b0b03edd6
Om Yadav. <159103737+omyadav11@users.noreply.github.com>
2024-08-07 22:20:11 -0700

    Add files via upload

---
+++ Month.java
@@ -0,0 +1,70 @@
+ class Month{
+	public static void main(String args[]){
+		int n=0;
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		
+			
+			switch(args[i]){
+				
+				case "0" :
+					System.out.println("jan");
+				break;
+				
+				case "1":
+					System.out.println("feb");
+				break;
+
+				case "2":
+					System.out.println("mar");
+				break;
+
+
+				case "3":
+					System.out.println("apr");
+				break;
+
+
+				case "4":
+					System.out.println("may");
+				break;
+
+
+				case "5":
+					System.out.println("jun");
+				break;
+
+
+				case "6":
+					System.out.println("jul");
+				break;
+
+				case "7":
+					System.out.println("aug");
+				break;
+
+				case "8":
+					System.out.println("sep");
+				break;
+
+				case "9":
+					System.out.println("oct");
+				break;
+
+				case "10":
+					System.out.println("nov");
+				break;
+
+				case "11":
+					System.out.println("dec");
+				break;
+
+
+				}
+		}
+	}
+}
+
+
+
+					
