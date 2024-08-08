6b6f99edf923007ae0720e8ddead831b86fa1e6e
Om Yadav. <159103737+omyadav11@users.noreply.github.com>
2024-08-07 22:19:28 -0700

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,51 @@
+ class Switch{
+	public static void main(String args[]){
+		int n=0;
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		
+		
+			
+			switch(args[i]){
+				
+				case "0" :
+					System.out.println("sunday");
+				break;
+				
+				case "1":
+					System.out.println("monday");
+				break;
+
+				case "2":
+					System.out.println("Tuesday");
+				break;
+
+
+				case "3":
+					System.out.println("wednesday");
+				break;
+
+
+				case "4":
+					System.out.println("thursday");
+				break;
+
+
+				case "5":
+					System.out.println("friday");
+				break;
+
+
+				case "6":
+					System.out.println("saturday");
+				break;
+
+
+				}
+		}
+	}
+}
+
+
+
+					
